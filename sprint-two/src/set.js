0930eebd14f79ee3d1c2c630fda6fce269c8b5bd
Hack Reactor Students <accounts@hackreactor.com>
2015-07-02 16:40:34 -0700

    almost there

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set.storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	if (this.storage.indexOf(item) === -1){
+		this.storage.push(item);
+	}
 };
 
 setPrototype.contains = function(item){
+	if (this.storage.indexOf(item) >= 0) {
+		return true;
+	}
+	return false;
 };
 
 setPrototype.remove = function(item){
+	var index = this.storage.indexOf(item);
+	this.storage = this.storage.slice(0, index).concat(this.storage.slice(index + 1));
 };
 
 /*
