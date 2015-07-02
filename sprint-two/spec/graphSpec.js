6c655f8399ae8deb40023cc34d997618e510b468
Hack Reactor Students <accounts@hackreactor.com>
2015-07-02 16:00:00 -0700

    graph done

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -40,11 +40,13 @@ describe('graph', function() {
     graph.addNode('apples');
     graph.addNode('satsumas');
     graph.addEdge('satsumas', 'apples');
-    expect(graph.hasEdge('apples', 'satsumas')).to.equal(true);
+    graph.removeEdge('satsumas', 'apples');
+    expect(graph.hasEdge('apples', 'satsumas')).to.equal(false);
   });
 
   it('should execute a callback on each node in the graph', function() {
     var connectToSatsumas = function(item) {
+      console.log(graph);
       graph.addEdge(item, 'satsumas');
     };
     graph.addNode('satsumas');
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,28 +1,90 @@
 
 
 var Graph = function(){
+	this.nodes = [];
 };
 
 Graph.prototype.addNode = function(node){
+	this.nodes.push({
+		value: node, 
+		pointers: []
+	});
+
 };
 
 Graph.prototype.contains = function(node){
+	for (var i = 0; i < this.nodes.length; i++) {
+		if (this.nodes[i].value === node) {
+			return true;
+		}
+	}
+	return false;
 };
 
 Graph.prototype.removeNode = function(node){
+	var numberOfNodes = this.nodes.length;
+	for (var i = 0; i < numberOfNodes; i++) {
+		if (this.nodes[i].value === node) {
+			this.nodes = this.nodes.slice(0, i).concat(this.nodes.slice(i + 1));
+		}
+	}
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+	for (var i = 0; i < this.nodes.length; i++) {
+		if (this.nodes[i].value === fromNode) {
+			var from = this.nodes[i];
+		}
+		if (this.nodes[i].value === toNode) {
+			var to = this.nodes[i];
+		}
+	}
+
+	if (from.pointers.indexOf(toNode) > -1) {
+		return true;
+	} else {
+		return false;
+	}
+
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+	for (var i = 0; i < this.nodes.length; i++) {
+		if (this.nodes[i].value === fromNode) {
+			var from = this.nodes[i];
+		}
+		if (this.nodes[i].value === toNode) {
+			var to = this.nodes[i];
+		}
+	}
+
+	from.pointers.push(toNode);
+	to.pointers.push(fromNode);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+	for (var i = 0; i < this.nodes.length; i++) {
+		if (this.nodes[i].value === fromNode) {
+			var from = this.nodes[i];
+		}
+		if (this.nodes[i].value === toNode) {
+			var to = this.nodes[i];
+		}
+	}
+	var fromIndex = from.pointers.indexOf(toNode);
+	from.pointers = from.pointers.slice(0, fromIndex).concat(from.pointers.slice(fromIndex + 1));
+
+	var toIndex = to.pointers.indexOf(fromNode);
+	to.pointers = to.pointers.slice(0, toIndex).concat(to.pointers.slice(toIndex + 1));
 };
 
 Graph.prototype.forEachNode = function(cb){
+	for (var i = 0; i < this.nodes.length; i++) {
+		cb(this.nodes[i].value);
+	}
 };
+
+// 
 
 /*
  * Complexity: What is the time complexity of the above functions?
